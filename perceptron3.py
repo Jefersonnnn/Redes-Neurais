0779690f7197f57773ad7adda3e722d0a1efbc6b
Jefersonnnn <jefersonmachado@univille.br>
2019-03-17 15:16:19 -0300

    OR

--- perceptron3.py
+++ perceptron3.py
@@ -7,8 +7,12 @@ Created on Sun Mar 17 14:33:20 2019
 
 import numpy as np
 #Tabela verdade operador E
+#entradas = np.array([[0,0],[0,1],[1,0],[1,1]])
+#saidas = np.array([0,0,0,1])
+
+#Tabela verdade operador OU
 entradas = np.array([[0,0],[0,1],[1,0],[1,1]])
-saidas = np.array([0,0,0,1])
+saidas = np.array([0,1,1,1])
 pesos = np.array([0.0,0.0])
 taxaAprendizagem = 0.1
 
@@ -21,6 +25,8 @@ def calculaSaida(registro):
     s = registro.dot(pesos)
     return stepfunction(s)
 
+#Encontrar os pesos
+#Minizando o erro
 def treinar():
     erroTotal = 1
     while (erroTotal != 0):
@@ -35,3 +41,8 @@ def treinar():
         print('Total de erros: ' + str(erroTotal))
 
 treinar()
+print('Rede neural treinada')
+print(calculaSaida(entradas[0]))
+print(calculaSaida(entradas[1]))
+print(calculaSaida(entradas[2]))
+print(calculaSaida(entradas[3]))
